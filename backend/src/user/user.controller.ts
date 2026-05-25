96c906a8832b370c581928505bc5e360a452f07c
FAT-CAT <ahosanh896@gmail.com>
2026-05-26 01:14:19 +0600

    add auth interface and user Controller

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ClerkAuthGuard } from 'src/auth/auth.guard';
+import type { AuthRequest } from 'src/auth/interfaces/auth-request.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  //sync user
+  @Post('sync')
+  @UseGuards(ClerkAuthGuard)
+  async syncUser(@Req() req: AuthRequest) {
+    return this.userService.createUser({
+      clerkId: req.user.clerkId,
+
+      email: req.user.email,
+
+      username: '',
+      imageUrl: '',
+    });
+  }
+
+  //get profile
+  @Get('me')
+  @UseGuards(ClerkAuthGuard)
+  async me(@Req() req: AuthRequest) {
+    return this.userService.getProfile(req.user.clerkId);
+  }
+}
