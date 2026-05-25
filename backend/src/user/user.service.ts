6c6d605267e28ece2d67ab3e877e58023ee339cb
FAT-CAT <ahosanh896@gmail.com>
2026-05-26 00:37:21 +0600

    add user service functionality

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schema/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  //for get the existing user
+  async findByClerkId(clerkId: string) {
+    return this.userModel.findOne({
+      clerkId,
+    });
+  }
+
+  async createUser(dto: CreateUserDto) {
+    const existingUser = await this.findByClerkId(dto.clerkId);
+
+    if (existingUser) {
+      return existingUser;
+    }
+
+    return this.userModel.create(dto);
+  }
+
+  async getProfile(clerkId: string) {
+    return this.findByClerkId(clerkId);
+  }
+}
