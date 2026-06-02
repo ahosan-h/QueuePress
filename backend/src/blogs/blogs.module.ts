7ddda7a787eb2d0d6ae42621e15244033ebf50ef
FAT-CAT <ahosanh896@gmail.com>
2026-06-02 22:35:14 +0600

    configure Blog schema in module

--- backend/src/blogs/blogs.module.ts
+++ backend/src/blogs/blogs.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BlogsController } from './blogs.controller';
 import { BlogsService } from './blogs.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Blog, BlogSchema } from './schema/blogs.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Blog.name,
+        schema: BlogSchema,
+      },
+    ]),
+  ],
   controllers: [BlogsController],
-  providers: [BlogsService]
+  providers: [BlogsService],
 })
 export class BlogsModule {}
