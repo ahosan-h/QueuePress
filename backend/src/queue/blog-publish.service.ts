5ee8402eeffb3845b48bb55690101721360d940c
FAT-CAT <ahosanh896@gmail.com>
2026-06-03 19:57:19 +0600

    add scheduled blog publishing via BullMQ queue

--- backend/src/queue/blog-publish.service.ts
+++ backend/src/queue/blog-publish.service.ts
@@ -1,4 +1,17 @@
+import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bullmq';
 
 @Injectable()
-export class BlogPublishService {}
+export class BlogPublishService {
+  constructor(
+    @InjectQueue('publish')
+    private readonly publishQueue: Queue,
+  ) {}
+
+  async schedulePost(blogId: string, scheduleAt: Date) {
+    const delay = new Date(scheduleAt).getTime() - Date.now();
+
+    return this.publishQueue.add('publish-blog', { blogId }, { delay });
+  }
+}
