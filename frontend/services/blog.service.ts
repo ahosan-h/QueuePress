8f1469cb6cb5a72209f1b7fe7c91cd3abc154a9a
FAT-CAT <ahosanh896@gmail.com>
2026-06-07 22:23:33 +0600

    add blog service CRUD methods

--- frontend/services/blog.service.ts
+++ frontend/services/blog.service.ts
@@ -1,15 +1,28 @@
 import { apiFetch } from "@/lib/api";
-import { Blog } from "@/types/blog";
+import { Blog, CreateBlogDto, UpdateBlogDto } from "@/types/blog";
 
 export const BlogService = {
   getAll: () => apiFetch<Blog[]>("/blogs"),
 
   getById: (id: string) => apiFetch<Blog>(`/blogs/${id}`),
 
-  create: (data: Partial<Blog>, token: string) =>
+  create: (data: CreateBlogDto, token: string) =>
     apiFetch<Blog>("/blogs", {
       method: "POST",
       token,
       body: data,
+    }),
+
+  update: (data: UpdateBlogDto, token: string, id: string) =>
+    apiFetch<Blog>(`/blogs/${id}`, {
+      method: "PATCH",
+      token,
+      body: data,
+    }),
+
+  remove: (id: string, token: string) =>
+    apiFetch(`/blogs/${id}`, {
+      method: "DELETE",
+      token,
     }),
 };
