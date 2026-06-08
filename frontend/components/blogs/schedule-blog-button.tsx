572732b99f213e0f8a9ce8b9ec7cba0632df8796
FAT-CAT <ahosanh896@gmail.com>
2026-06-08 16:48:31 +0600

    implement SheduleBlogButton

---
+++ frontend/components/blogs/schedule-blog-button.tsx
@@ -0,0 +1,18 @@
+import { useAuthAction } from "@/hooks/use-auth-action";
+import { Button } from "../ui/button";
+import { CalendarDays } from "lucide-react";
+
+interface Props {
+  onSchedule: () => void;
+}
+
+export function SheduleBlogButton({ onSchedule }: Props) {
+  const { requireAuth } = useAuthAction();
+
+  return (
+    <Button onClick={() => requireAuth(onSchedule)}>
+      <CalendarDays className="mr-2 h-4 w-4" />
+      Schedule
+    </Button>
+  );
+}
